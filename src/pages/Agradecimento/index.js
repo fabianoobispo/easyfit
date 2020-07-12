87a58ed33079b8b998551551221f2c6684c7e472
fsbianoobispo <fbc623@gmail.com>
2020-07-11 23:09:08 -0300

    editando pagina agradecimento

--- src/pages/Agradecimento/index.js
+++ src/pages/Agradecimento/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import kettleBell from '../../assets/Kettlebell.png';
+
 import { Container } from './styles';
 
 function Agradecimento() {
     return (
         <Container>
-            <h1>Agradecimento</h1>
+            <img src={kettleBell} />
+            <h1>Obrigado pelo interesse!</h1>
+            <span>
+                Nossos consultores entrarão em contato o mais rápido possível.
+            </span>
+            <span>Enquanto isso, siga o EasyFit no instagram</span>
         </Container>
     );
 }
--- src/pages/Agradecimento/styles.js
+++ src/pages/Agradecimento/styles.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
+import background from '../../assets/BackgroundAgradecimento.png';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    background-image: url(${background});
+    width: 1920px;
+    height: 1032px;
+`;
