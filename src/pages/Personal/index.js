047abf45d0c5d6233a2d7406e0490e688b17fec6
fsbianoobispo <fbc623@gmail.com>
2020-07-20 21:22:04 -0300

    s

--- src/pages/Personal/index.js
+++ src/pages/Personal/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import { Container, Div1, Div2, Div3, Div4 } from './styles';
+import { Container, Div1, Div2, Div3 } from './styles';
 
 import LogoBranca from '../../assets/LogoCabecalhoBranca.png';
+import LogoCabecalho from '../../assets/LogoCabecalho.png';
 import Check from '../../assets/VetorCheck.png';
+
+import Kettlebell from '../../assets/KETTLEBELL1.png';
+
+import Card01 from '../../assets/quadro01.png';
+import Card02 from '../../assets/quadro02.png';
 
 function Personal() {
     return (
         <Container>
             <Div1>
-                <img src={LogoBranca} src={LogoBranca} />
+                <img src={LogoBranca} alt={LogoBranca} />
 
                 <titulo>
                     <p>
@@ -18,7 +24,7 @@ function Personal() {
                         <br /> novo mundo
                         <br /> <strong>com EasyFit</strong>
                     </p>
-                    <img src={Check} src={Check} />
+                    <img src={Check} alt={Check} />
                 </titulo>
 
                 <subTitulo>
@@ -35,13 +41,33 @@ function Personal() {
                 <p>
                     <strong>O mundo mudou.</strong> E vai continuar mudando.
                 </p>
+                <cards>
+                    <img src={Card01} alt="Card01" />
+                    <img src={Card02} alt="Card02" />
+                </cards>
+
+                <img src={Kettlebell} alt="Kettlebell" />
             </Div2>
             <Div3>
-                <h1>Div3</h1>
+                <form>
+                    <p>
+                        Quero me cadastrar
+                        <br />
+                        gratuitamnete
+                    </p>
+
+                    <input name="name" placeholder="Nome..." />
+                    <input name="email" placeholder="E-mail..." />
+                    <input name="Tel" placeholder="Telefone..." />
+
+                    <button>Confirmar cadastro</button>
+                </form>
+
+                <logo>
+                    <img src={LogoCabecalho} alt="LogoCabecalho" />
+                    <p>Siga nas redes sociais.</p>
+                </logo>
             </Div3>
-            <Div4>
-                <h1>Div4</h1>
-            </Div4>
         </Container>
     );
 }
--- src/pages/Personal/styles.js
+++ src/pages/Personal/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import backgroundDiv1 from '../../assets/BackgroudPersonal.png';
+import backgroundDiv6 from '../../assets/BackgroundCadastroPersonal.png';
 
 export const Container = styled.div`
     margin: 0 auto;
@@ -12,14 +13,18 @@ export const Div1 = styled.div`
     width: 100%;
     height: 1040px;
 
+    img {
+        max-width: 239px;
+        max-height: 94px;
+
+        top: 70px;
+    }
+
     titulo {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
         p {
             font-family: 'Anton', sans-serif;
             font-size: 60px;
-            text-align: left;
+
             color: #fff;
         }
         img {
@@ -49,11 +54,97 @@ export const Div1 = styled.div`
     }
 `;
 export const Div2 = styled.div`
-    background-color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    p {
+        font-family: 'Anton', sans-serif;
+        font-size: 60px;
+        color: #000;
+    }
+    cards {
+        display: flex;
+        flex-direction: row;
+        margin-top: 40px;
+        img {
+            padding: 20px;
+            height: 645px;
+            width: 730px;
+        }
+    }
+    img {
+        height: 278px;
+        width: 324px;
+        margin-bottom: 60px;
+    }
 `;
 export const Div3 = styled.div`
-    background-color: red;
-`;
-export const Div4 = styled.div`
-    background-color: black;
+    background-image: url(${backgroundDiv6});
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 730px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    form {
+        display: flex;
+        flex-direction: column;
+
+        margin-top: 20px;
+        margin-left: 20%;
+        width: 450px;
+
+        p {
+            font-family: 'Anton', sans-serif;
+            font-size: 40px;
+            text-align: center;
+            color: #fff;
+        }
+
+        input {
+            margin-top: 15px;
+            background-color: #9b149d;
+            border: none;
+            border-radius: 10px;
+            color: #fff;
+            font-family: sans-serif;
+            font-size: 20px;
+
+            padding: 10px;
+        }
+        button {
+            font-family: 'Roboto', sans-serif;
+            font-size: 20px;
+            padding: 10px;
+            margin-top: 15px;
+            border-radius: 10px;
+            border: none;
+            color: #fff;
+            background-color: #4fc580;
+            width: 100%;
+        }
+    }
+
+    logo {
+        display: flex;
+        flex-direction: column;
+
+        margin-top: 120px;
+        margin-left: 30%;
+        width: 450px;
+
+        img {
+            width: 160px;
+            height: 50px;
+        }
+
+        p {
+            font-family: 'Roboto', sans-serif;
+            font-size: 20px;
+            margin-top: 10px;
+            color: #fff;
+        }
+    }
 `;
